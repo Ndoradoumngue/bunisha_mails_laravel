ccd2f08ee7f4dde743528063678028a781cc987e
ndoradoumngue <cndoradoumngue@gmail.com>
2020-08-21 00:03:58 +0300

    last updates

--- resources/views/pages/introductoryemail.blade.php
+++ resources/views/pages/introductoryemail.blade.php
@@ -4,78 +4,89 @@
 
 	<div class="body-div">
 
-        <!-- <div class="introductory-title">
-            <img src="https://kampuni.s3.amazonaws.com/assets/Email-Design-introductory-Banner.jpg">
-            <h2 class="introductory-mail-start">Welcome</h2>
-            <p class="to">to</p>
-            <p class="introductory-sub-title">Your e-Business Platform</p>
-        </div> -->
+                <!-- <div class="introductory-title">
+                    <img src="https://kampuni.s3.amazonaws.com/assets/Email-Design-introductory-Banner.jpg">
+                    <h2 class="introductory-mail-start">Welcome</h2>
+                    <p class="to">to</p>
+                    <p class="introductory-sub-title">Your e-Business Platform</p>
+                </div> -->
 
-        <img class="starting-introductory-email" src="https://kampuni.s3.amazonaws.com/assets/Email-Design-introductory-Banner.jpg">
+                <img class="starting-introductory-email" src="https://kampuni.s3.amazonaws.com/assets/Email-Design-introductory-Banner.jpg">
 
-        <div class="centered">
-            <br/><br/>
-            <a href="https://www.bunisha.com/services/tenders" class="view-in-browser-button">View in your browser</a>
-            <br/>
-        </div>
+                <div class="centered">
+                    <br/><br/>
+                    <a href="https://www.bunisha.com/services/tenders" class="view-in-browser-button">View in your browser</a>
+                    <br/>
+                </div>
 
-        <div class="automating"><br/>We are automating all your business process.</div>
+                <div class="automating"><br/>We are automating all your business process.</div>
 
-        <div class="centered">
-            <br/>
-            <!-- <h2 class="suggestion-title">We thought you might be interested in:</h2> -->
-            <h2 class="tenders-list">Tenders</h2>
-        </div>
+                <div class="centered">
+                    <br/>
+                    <!-- <h2 class="suggestion-title">We thought you might be interested in:</h2> -->
+                    <h2 class="tenders-list">Tenders</h2>
+                </div>
 
-        <div class="waiting-section">
-            <p>
-                <br/>
-                We have automated the tendering process to a click. Upon settling up your company on  Bunisha platform you will be able to compile your tender within <b>15mn</b>.
-            </p>
+                <div class="waiting-section">
+                    <p>
+                        <br/>
+                        We have automated the tendering process to a click. Upon settling up your company on  Bunisha platform you will be able to compile your tender within <b>15mn</b>.
+                    </p>
 
-            <img src="https://kampuni.s3.amazonaws.com/assets/shutterstock_1017688345.jpg">
+                    <img src="https://kampuni.s3.amazonaws.com/assets/shutterstock_1017688345.jpg">
 
-            <div class="centered">
-            <h2 class="question-title">DO YOU DO TENDERS?</h2>
-                <a href="https://www.bunisha.com/" class="yes-button">YES</a>
-                <a href="https://www.bunisha.com/" class="no-button">NO</a>
+                    <div class="centered">
+                    <h2 class="question-title">DO YOU DO TENDERS?</h2>
+                        <!-- <a href="https://www.bunisha.com/" class="yes-button">YES</a>
+                        <a href="https://www.bunisha.com/" class="no-button">NO</a> -->
+                        <div class="centered">
+                            <br/>
+                            <a href="https://www.bunisha.com/services/tenders" class="view-more-button">&nbsp;&nbsp;&nbsp;&nbsp; View More &nbsp;&nbsp;&nbsp;&nbsp;</a>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+                <br/><br/>
+
+                <div class="pre-qualification-section">
+                    <h2 class="pre-qualification-title">PRE-QUALIFICATION</h2>
+                    <img src="https://kampuni.s3.amazonaws.com/assets/2479440.jpg">
+                    <p>
+                        <br/>
+                        Do you want your company to be <span class="prequalified">pre-qualified</span> in 10mn procuring entities evevry month.
+                    </p>
+                    <div class="centered">
+                        <!-- <a href="https://www.bunisha.com/"><button class="pre-qualification-yes-button">YES</button></a>
+                        <a href="https://www.bunisha.com/"><button class="pre-qualification-maybe-button">MAYBE</button></a> -->
+                        <br/>
+                        <a href="https://www.bunisha.com/services/tenders" class="view-in-browser-button">&nbsp;&nbsp;&nbsp;&nbsp; Read More &nbsp;&nbsp;&nbsp;&nbsp;</a>
+                        <br/>
+                    </div>
+                </div>
+
+                <div class="waiting-section">
+                    <br/>
+                    <h2 class="e-tender-platform">e-Tender Platform</h2>
+                    <p>
+                        Kenya number 1 e-tender platform where you get over 200 new opportunities every day.<br/>
+                        <span class="blue-color"> Tender Notices, Pre-qualifications, Tender results, Tender sites visits, Addendumbs And Awards</span>.
+                    </p>
+                    <h2 class="notifications-title">Notifications</h2>
+
+                    <img src="https://kampuni.s3.amazonaws.com/assets/1576.jpg">
+
+                    <p>
+                        Do you want to get <span class="daily-notifications">daily customized</span> notifications?.
+                    </p>
+
+                    <div class="centered">
+                        <!-- <a href="https://www.bunisha.com/"><button class="subscribe-button">Subscribe</button></a> -->
+                        <br/>
+                        <a href="https://www.bunisha.com/services/tenders" class="view-more-button">&nbsp;&nbsp;&nbsp;&nbsp; Subscribe &nbsp;&nbsp;&nbsp;&nbsp;</a>
+                        <br/>
+                    </div>
+                </div>
+
             </div>
-        </div>
-        <br/><br/>
-
-        <div class="pre-qualification-section">
-            <h2 class="pre-qualification-title">PRE-QUALIFICATION</h2>
-            <img src="https://kampuni.s3.amazonaws.com/assets/2479440.jpg">
-            <p>
-                <br/>
-                Do you want your company to be <span class="prequalified">pre-qualified</span> in 10mn procuring entities evevry month.
-            </p>
-            <div class="centered">
-                <a href="https://www.bunisha.com/"><button class="pre-qualification-yes-button">YES</button></a>
-                <a href="https://www.bunisha.com/"><button class="pre-qualification-maybe-button">MAYBE</button></a>
-            </div>
-        </div>
-
-        <div class="waiting-section">
-            <br/>
-            <h2 class="e-tender-platform">e-Tender Platform</h2>
-            <p>
-                Kenya number 1 e-tender platform where you get over 200 new opportunities every day.<br/>
-                <span class="blue-color"> Tender Notices, Pre-qualifications, Tender results, Tender sites visits, Addendumbs And Awards</span>.
-            </p>
-            <h2 class="notifications-title">Notifications</h2>
-
-            <img src="https://kampuni.s3.amazonaws.com/assets/1576.jpg">
-
-            <p>
-                Do you want to get <span class="daily-notifications">daily customized</span> notifications?.
-            </p>
-
-            <div class="centered">
-                <a href="https://www.bunisha.com/"><button class="subscribe-button">Subscribe</button></a>
-            </div>
-        </div>
-
-    </div>
 
 @endsection
